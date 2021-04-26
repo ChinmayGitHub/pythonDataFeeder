5c942ad805f24248664054c3719219abddf39fd9
Chinmay Samant <chinmaysamant@MacBook-Pro-16.local>
2021-04-26 18:24:09 +0530

    apiFile

--- api/api.py
+++ api/api.py
@@ -7,4 +7,4 @@ app.config["DEBUG"] = True
 @app.route('/getDivision', methods=['GET'])
 def home():
     return jsonify({'division': 'Labs'})
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', debug=True, port=5000)
